8c174c5799da1414e3723b203cec212cc4ef42c1
rafat.hsn@gmail.com <rafat.hsn@gmail.com@205e1aae-37eb-8f23-b376-a47c25254588>
2011-07-17 21:41:15 +0000

    add

---
+++ dyadwaves2d/main.cpp
@@ -0,0 +1,37 @@
+#include <QtGui>
+#include "mainwindow.h"
+
+/*Copyright (C) 2011  Rafat Hussain
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  See COPYING.LESSER and COPYING files that come
+ * with the software.
+*/
+
+
+using namespace std;
+extern "C" int _get_output_format( void ){ return 0; }
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+ //   QLibrary library("C:/Documents and Settings/USER/My Documents/Downloads/qtprojects/wavelet2-build-desktop/debug/libwavelet2.a");
+ //   library.load();
+    MainWindow w;
+    w.setWindowState(Qt::WindowMaximized);
+
+    w.show();
+
+    return a.exec();
+}
+
